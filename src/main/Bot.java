b16167cea3202743f85253741a46c9f2ebb99cf4
joacotol <joacochrist@gmail.com>
2022-03-01 16:11:15 -0500

    Implemented test() method in Bot class

--- src/main/Bot.java
+++ src/main/Bot.java
@@ -1,4 +1,5 @@
 package main;
+import java.util.Scanner;
 public class Bot {
 
     private String name;
@@ -34,5 +35,35 @@ public class Bot {
             System.out.printf("%d!\n", i);
         }
         System.out.println("Completed, have a nice day!");
+    }
+
+    // Multiple Choice
+    public void test() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.\n" +
+                "2. To decompose a program into several small subroutines.\n" +
+                "3. To determine the execution time of a program.\n" +
+                "4. To interrupt the execution of a program.\n");
+
+        while (true) {
+            int input = userInput.nextInt();
+
+            switch (input) {
+                case 1:
+                case 3:
+                case 4:
+                    System.out.println("Please, try again.");
+                    break;
+                case 2:
+                    break;
+            }
+            if (input == 2) {
+                System.out.println("Congratulations, have a nice day!");
+                break;
+            }
+        }
+
     }
 }
