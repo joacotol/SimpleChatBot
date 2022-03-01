c7bb7d6d8b36abfeaf9448405e8bd9e62c1da0f6
joacotol <joacochrist@gmail.com>
2022-03-01 14:59:46 -0500

    Added private User instance variable to UserInterface class

--- src/main/UserInterface.java
+++ src/main/UserInterface.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 public class UserInterface {
     private Scanner scanner;
     private Bot bot;
+    private User user;
 
-    public UserInterface(Scanner scanner, Bot bot) {
+    public UserInterface(Scanner scanner, Bot bot, User user) {
         this.scanner = scanner;
         this.bot = bot;
+        this.user = user;
     }
 
     public void start() {
         System.out.printf("Hello! My name is %s.\n", this.bot.getName());
         System.out.printf("I was created in %d.\n", this.bot.getBirthYear());
+        System.out.println("Please, remind me your name.");
     }
 
 }
