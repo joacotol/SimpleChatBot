ecf06151a36b47a584561e43e32828a33f40c883
joacotol <joacochrist@gmail.com>
2022-03-01 15:25:02 -0500

    Added instance variable age in the User class

--- src/main/User.java
+++ src/main/User.java
@@ -2,13 +2,20 @@ package main;
 
 public class User {
     private String name;
+    private int age;
 
-    public User(String name) {
+    public User(String name, int age) {
         this.name = name;
+        this.age = age;
     }
 
     public User() {
         this.name = "Default";
+        this.age = 0;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public void setName(String name) {
@@ -17,5 +24,9 @@ public class User {
 
     public String getName() {
         return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
     }
 }
