9f5b1d7560d26e5c7a115cf10db8b6091785b1db
joacotol <joacochrist@gmail.com>
2022-03-01 14:14:35 -0500

    Made a basic output for the chatbot

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,15 @@
 package main;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // main
+
+        Scanner scanner = new Scanner(System.in);
+        Bot bot = new Bot("Aid", 2020);
+        UserInterface ui = new UserInterface(scanner, bot);
+
+        ui.start();
+
     }
 }
